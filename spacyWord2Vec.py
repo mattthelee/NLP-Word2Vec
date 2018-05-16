6e68eeda1a84a25bdf148002b2dfb59e47cc57eb
Matthew Lee <matt_lee92@hotmail.co.uk>
2018-05-16 09:05:48 +0100

    Add basic retry loop

--- spacyWord2Vec.py
+++ spacyWord2Vec.py
@@ -1,6 +1,15 @@
-import spacy 
-
+import spacy
 import numpy as np
 import matplotlib.pyplot as plt
+
 nlp = spacy.load('en_core_web_lg')
-print (nlp('dog').vector)
+repeat = True
+while repeat:
+    repeat = False
+
+    exampleWord = input("What word would you like to convert to a vector? \n")
+    print ("Here is ",exampleWord," as a vector")
+    print (nlp('dog').vector)
+    repeat = (input("\nWould you like to try another word? y/n\n") == "y")
+
+print ("Now that we are able to turn words into vectors, we can start manipulating them")
